466a39a3a1f34ac74da15b516690d386e8714218
Mauricio Junior <mjuniior92@gmail.com>
2016-02-10 23:07:45 -0200

    config browserify

--- react-flux/psadmin/gulpfile.js
+++ react-flux/psadmin/gulpfile.js
@@ -3,17 +3,24 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var open = require('gulp-open');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var rename = require('gulp-rename');
+var streamify = require('gulp-streamify');
+var uglify = require('gulp-uglify');
 
 var config = {
 	port: 9005,
 	devBaseUrl: 'http://localhost',
 	paths: {
 		html: './src/*.html',
+		js: './src/**/*.js',
 		dist: './dist'
 	}
 };
 
-gulp.task('connect', function() {
+gulp.task('connect', () => {
 	connect.server({
 		root: ['dist'],
 		port: config.port,
@@ -22,19 +29,29 @@ gulp.task('connect', function() {
 	});
 });
 
-gulp.task('open', ['connect'],  function() {
+gulp.task('open', ['connect'], () => {
 	gulp.src('dist/index.html')
 		.pipe(open({ uri: config.devBaseUrl + ':' + config.port }));
 });
 
-gulp.task('html', function() {
+gulp.task('html', () => {
 	gulp.src(config.paths.html)
 		.pipe(gulp.dest(config.paths.dist))
 		.pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-	gulp.watch(config.paths.html, ['html']);
+gulp.task('js', () => {
+	var bundleStream = browserify('./src/main.js').bundle();
+	bundleStream
+		.pipe(source('main.js'))
+		.pipe(streamify(uglify()))
+		.pipe(rename('bundle.js'))
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('watch', () => {
+	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'open', 'watch'], () => console.log(config.paths.js));
---
+++ react-flux/psadmin/src/main.js
@@ -0,0 +1,3 @@
+var App = console.log('Hello world from Browserify!!!!!!!!!!!!11');
+
+module.exports = App;
