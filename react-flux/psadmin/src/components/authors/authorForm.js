929e6f64ccab8de768e310c0decb03b9c54f71e6
Mauricio Junior <mjuniior92@gmail.com>
2016-02-24 00:08:41 -0300

    add input component

--- react-flux/psadmin/src/components/authors/authorForm.js
+++ react-flux/psadmin/src/components/authors/authorForm.js
@@ -1,31 +1,24 @@
 'use strict';
 
 import React from 'react';
+import Input from '../commons/textInput';
 
 const AuthorForm = React.createClass({
 	render() {
 		return (
 			<form>
-				<label htmlFor="firstName">First Name</label>
-				<input type="text"
-					name="firstName"
-					className="form-control"
-					placeholder="First Name"
-					ref="firstName"
-					onChange = { this.props.onChange }
-					value = { this.props.author.firstName }
+				<Input
+					name='firstName'
+					label='First Name'
+					value={ this.props.author.firstName }
+					onChange={ this.props.onChange }
 				/>
-				<br />
-				<label htmlFor="lastName">Last Name</label>
-				<input type="text"
-					name="lastName"
-					className="form-control"
-					placeholder="First Name"
-					ref="lastName"
-					onChange = { this.props.onChange }
-					value = { this.props.author.lastName }
+				<Input
+					name='lastName'
+					label='Last Name'
+					value={ this.props.author.lastName }
+					onChange={ this.props.onChange }
 				/>
-				<br />
 				<input type="submit" value="Save" className="btn btn-default" />
 			</form>
 		);
