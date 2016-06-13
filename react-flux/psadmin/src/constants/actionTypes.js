d9d84ff839f763fcb8004473a49d43cce5e302cc
Maurício Júnior <mjuniior92@gmail.com>
2016-06-12 23:47:39 -0300

    Update actionTypes.js

--- react-flux/psadmin/src/constants/actionTypes.js
+++ react-flux/psadmin/src/constants/actionTypes.js
@@ -1,8 +1,8 @@
-import keyMirror from 'keymirror';
+import keyMirror from 'keymirror'
 
 module.exports = keyMirror({
 	INITIALIZE: null,
 	CREATE_AUTHOR: null,
 	UPDATE_AUTHOR: null,
 	UDELETE_AUTHOR: null
-});
+})
