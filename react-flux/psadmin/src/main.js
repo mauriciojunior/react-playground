366241f1005d77d7b2f7ab009042c4eb112184bc
Maurício Júnior <mjuniior92@gmail.com>
2016-06-12 23:29:12 -0300

    Update main.js

--- react-flux/psadmin/src/main.js
+++ react-flux/psadmin/src/main.js
@@ -1,8 +1,8 @@
-'use strict';
+'use strict'
 
 import React from 'react'
-import { render } from 'react-dom'
-import { Router, Route, IndexRoute, NotFoundRoute, hashHistory } from 'react-router'
+import {render} from 'react-dom'
+import {Router, Route, IndexRoute, NotFoundRoute, hashHistory} from 'react-router'
 import App from './components/app'
 import Home from './components/homePage'
 import Authors from './components/authors/authorsPage'
@@ -12,17 +12,17 @@ import ManageAuthorPage from './components/authors/manageAuthorPage'
 import $ from 'jquery'
 import InitializeActions from './actions/initializeActions'
 
-InitializeActions.initApp();
+InitializeActions.initApp()
 
 render((
-	<Router history={ hashHistory }>
-		<Route path='/' component={ App }>
-			<IndexRoute component={ Home } />
-			<Route path='authors' component={ Authors } />
-			<Route path='about' component={ About } />
-			<Route path='addAuthor' component={ ManageAuthorPage } />
-			<Route path='author/:id' component={ ManageAuthorPage }/>
-			<Route path='*' component={ NotFoundPage } />
+	<Router history={hashHistory}>
+		<Route path='/' component={App}>
+			<IndexRoute component={Home} />
+			<Route path='authors' component={Authors} />
+			<Route path='about' component={About} />
+			<Route path='addAuthor' component={ManageAuthorPage} />
+			<Route path='author/:id' component={ManageAuthorPage}/>
+			<Route path='*' component={NotFoundPage} />
 		</Route>
 	</Router>), document.querySelector('[data-js="app"]')
-);
+)
