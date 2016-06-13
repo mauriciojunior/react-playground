124de578c0843acfdfe57d112feaca01718f41cb
Maurício Júnior <mjuniior92@gmail.com>
2016-06-12 23:39:13 -0300

    Update authorStore.js

--- react-flux/psadmin/src/stores/authorStore.js
+++ react-flux/psadmin/src/stores/authorStore.js
@@ -1,57 +1,57 @@
-import Dispatcher from '../dispatcher/appDispatcher';
-import ActionTypes from '../constants/actionTypes';
-import { EventEmitter } from 'events';
-import assign from 'object-assign';
-import _ from 'lodash';
-const CHANGE_EVENT = 'change';
+import Dispatcher from '../dispatcher/appDispatcher'
+import ActionTypes from '../constants/actionTypes'
+import {EventEmitter} from 'events'
+import assign from 'object-assign'
+import _ from 'lodash'
+const CHANGE_EVENT = 'change'
 
-let _authors = [];
+let _authors = []
 
 const AuthorStore = assign({}, EventEmitter.prototype, {
-	addChangeListener(callback) {
-		this.on(CHANGE_EVENT, callback);
+	addChangeListener (callback) {
+		this.on(CHANGE_EVENT, callback)
 	},
 
-	removeChangeListener(callback) {
-		this.removeListener(CHANGE_EVENT, callback);
+	removeChangeListener (callback) {
+		this.removeListener(CHANGE_EVENT, callback)
 	},
 
-	emitChange() {
+	emitChange () {
 		this.emit(CHANGE_EVENT)
 	},
 
-	getAllAuthors() {
-		return _authors;
+	getAllAuthors () {
+		return _authors
 	},
 
-	getAuthorById(id) {
-		return _.find(_authors, {id: id});
+	getAuthorById (id) {
+		return _.find(_authors, {id: id})
 	}
-});
+})
 
 Dispatcher.register((action) => {
-	switch(action.actionType) {
+	switch (action.actionType) {
 		case ActionTypes.INITIALIZE:
-			_authors = action.initialData.authors;
-			AuthorStore.emitChange();
-			break;
+			_authors = action.initialData.authors
+			AuthorStore.emitChange()
+			break
 		case ActionTypes.CREATE_AUTHOR:
-			_authors.push(action.author);
-			AuthorStore.emitChange();
-			break;
+			_authors.push(action.author)
+			AuthorStore.emitChange()
+			break
 		case ActionTypes.UPDATE_AUTHOR:
-			let existingAuthor = _.find(_authors, { id: action.author.id });
-			let existingAuthorIndex = _.indexOf(_authors, existingAuthor);
-			_authors.splice(existingAuthorIndex, 1, action.author);
-			AuthorStore.emitChange();
-			break;
+			let existingAuthor = _.find(_authors, { id: action.author.id })
+			let existingAuthorIndex = _.indexOf(_authors, existingAuthor)
+			_authors.splice(existingAuthorIndex, 1, action.author)
+			AuthorStore.emitChange()
+			break
 		case ActionTypes.DELETE_AUTHOR:
 			_.remove(_authors, (author) => {
-				return action.id === author.id;
-			});
-			AuthorStore.emitChange();
-			break;
+				return action.id === author.id
+			})
+			AuthorStore.emitChange()
+			break
 	}
-});
+})
 
-module.exports = AuthorStore;
+module.exports = AuthorStore
