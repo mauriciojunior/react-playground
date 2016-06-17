4bb014771506c0bc611903660c8fc8b08c3e7489
Mauricio Junior <mjuniior92@gmail.com>
2016-06-17 00:48:32 -0300

    testing thunk

--- react-redux/src/actions/courseActions.test.js
+++ react-redux/src/actions/courseActions.test.js
@@ -30,10 +30,19 @@ const middleware = [thunk]
 const mockStore = configurationMockStore(middleware)
 
 describe('Asyn Actions', () => {
+	afterEach(() => nock.cleanAll())
 	it('should create BEGIN_AJAX_CALL and LOAD_COURSE_SUCCESS when loading courses', (done) => {
 		const expectedAction = [
-		{ type: types.BEGIN_AJAX_CALL }
-		{ type: types.LOAD_COURSE_SUCCESS, body: { courses: [{ id: 'clean-code', title: 'Clean Code' }] } }
+			{ type: types.BEGIN_AJAX_CALL },
+			{ type: types.LOAD_COURSE_SUCCESS, body: { courses: [{ id: 'clean-code', title: 'Clean Code' }] } }
 		]
+
+		const store = mockStore({ courses: [] }, expectedAction)
+		store.dispatch(courseActions.loadCourses()).then(() => {
+			const actions = store.getActions()
+			expect(actions[0].type).toEqual(types.BEGIN_AJAX_CALL)
+			expect(actions[1].type).toEqual(types.LOAD_COURSE_SUCCESS)
+		})
+		done()
 	})
 })
