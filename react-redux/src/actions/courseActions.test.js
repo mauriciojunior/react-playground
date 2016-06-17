f938dba071736f35b3c3c5295da59399fab71e48
Mauricio Junior <mjuniior92@gmail.com>
2016-06-16 23:59:35 -0300

    testing thunk

--- react-redux/src/actions/courseActions.test.js
+++ react-redux/src/actions/courseActions.test.js
@@ -2,6 +2,10 @@ import expect from 'expect'
 import * as courseActions from './courseActions'
 import * as types from './actionTypes'
 
+import thunk from 'redux-thunk'
+import nock from 'nock'
+import configurationMockStore from 'redux-mock-store'
+
 // Test a sync action
 describe('Course Actions', () => {
 	describe('createCourseSuccess', () => {
@@ -19,5 +23,17 @@ describe('Course Actions', () => {
 			// assert
 			expect(action).toEqual(expectedAction)
 		})
+	})
+})
+
+const middleware = [thunk]
+const mockStore = configurationMockStore(middleware)
+
+describe('Asyn Actions', () => {
+	it('should create BEGIN_AJAX_CALL and LOAD_COURSE_SUCCESS when loading courses', (done) => {
+		const expectedAction = [
+		{ type: types.BEGIN_AJAX_CALL }
+		{ type: types.LOAD_COURSE_SUCCESS, body: { courses: [{ id: 'clean-code', title: 'Clean Code' }] } }
+		]
 	})
 })
